85cc67a792852bde6f14d3ff29f7d0fce9c92225
Nishthakumari <knishtha0@gmail.com>
2023-05-25 00:11:08 +0530

    New Maven Project | Adding Spring Dependencies | Create Config File | Setter Injection

---
+++ src/main/java/com/springcore/SpringCoreApplication.java
@@ -0,0 +1,16 @@
+package com.springcore;
+
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringCoreApplication {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("Hello World");
+
+		new ClassPathXmlApplicationContext()
+	}
+
+}
