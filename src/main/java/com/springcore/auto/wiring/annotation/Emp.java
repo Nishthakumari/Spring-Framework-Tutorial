02863f623c00b23140730e85584c6822901cec53
Nishthakumari <knishtha0@gmail.com>
2023-05-27 12:13:32 +0530

    @Autowired Annotation for Autowiring

--- src/main/java/com/springcore/auto/wiring/annotation/Emp.java
+++ src/main/java/com/springcore/auto/wiring/annotation/Emp.java
@@ -1,22 +1,29 @@
-package com.springcore.auto.wiring;
+package com.springcore.auto.wiring.annotation;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class Emp {
+
 
     private Address address;
 
     public Emp() {
     }
 
+    @Autowired
     public Emp(Address address) {
-
+        
         this.address = address;
+        System.out.println("Inside constructor");
     }
 
     public Address getAddress() {
         return address;
     }
 
+
     public void setAddress(Address address) {
+        System.out.println("Setting value");
         this.address = address;
     }
 
--- src/main/java/com/springcore/auto/wiring/annotation/Test.java
+++ src/main/java/com/springcore/auto/wiring/annotation/Test.java
@@ -1,4 +1,4 @@
-package com.springcore.auto.wiring;
+package com.springcore.auto.wiring.annotation;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
