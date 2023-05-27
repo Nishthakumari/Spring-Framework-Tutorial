9662d7b371f445fd4176bf17395d302c9043a8a4
Nishthakumari <knishtha0@gmail.com>
2023-05-27 10:05:45 +0530

    Implementing Bean LifeCyle using interfaces

--- src/main/java/com/springcore/lifecycle/Test.java
+++ src/main/java/com/springcore/lifecycle/Test.java
@@ -6,8 +6,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test {
     public static void main(String[] args) {
         AbstractApplicationContext context = new ClassPathXmlApplicationContext("lifecycleconfig.xml");
+        context.registerShutdownHook();
+
         Samosa s1= (Samosa) context.getBean("s1");
         System.out.println(s1);
-        context.registerShutdownHook();
+
+
+        Pepsi p1 = (Pepsi) context.getBean("p1");
+        System.out.println(p1);
     }
 }
